46691f7288e83242797519117cb75e4b0749cf7f
zqjo <zqjo@fresnou.org>
2022-12-02 11:36:02 -0800

    working on pt. 3! :D

---
+++ models/card.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Card extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Card.init({
+    name: DataTypes.STRING,
+
+
+  }, {
+    sequelize,
+    modelName: 'Card',
+    tableName: 'cards',
+    timestamps: false
+  });
+  return Card;
+};
