4f818266fab56236f015cae5e741c99f9a9c9ade
zqjo29 <zqjo@fresnou.org>
2022-12-07 19:26:02 -0800

    mostly finished pt. 4! :D (just need to figure out how connect the pictures to the types) (and also fix hp and make hp (the variable) an integer)

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -6,6 +6,116 @@ module.exports.viewAll = async function(req, res, next) {
     res.render('index', {cards});
 };
 
+module.exports.renderEditForm = async function(req, res, next) {
+    const card = await Card.findByPk(
+        req.params.id
+    );
+    res.render('edit', {card, types});
+}
+
+module.exports.updateCard = async function(req, res) {
+    await Card.update(
+        {
+            name: req.body.name,
+            hp: req.body.hp,
+            type: req.body.type,
+            icon: req.body.icon,
+            picture: req.body.picture,
+            pop: req.body.pop,
+            move_one: req.body.move_one,
+            dam_one: req.body.dam_one,
+            desc_one: req.body.desc_one,
+            en_one_on: req.body.en_one_on,
+            en_one_tw: req.body.en_one_tw,
+            en_one_th: req.body.en_one_th,
+            move_two: req.body.move_two,
+            dam_two: req.body.dam_two,
+            desc_two: req.body.desc_two,
+            en_two_on: req.body.en_two_on,
+            en_two_tw: req.body.en_two_tw,
+            en_two_th: req.body.en_two_th,
+            weak: req.body.weak,
+            str: req.body.str,
+            retreat: req.body.retreat,
+            description: req.body.description
+        },
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
+
+module.exports.deleteCard = async function(req, res) {
+    await Card.destroy(
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
+
+module.exports.renderAddForm = function(req, res) {
+    const card = {
+        name: "",
+        hp: "",
+        type: types[0],
+        icon: "",
+        picture: "",
+        pop: "",
+        move_one: "",
+        dam_one: "",
+        desc_one: "",
+        en_one_on: "",
+        en_one_tw: "",
+        en_one_th: "",
+        move_two: "",
+        dam_two: "",
+        desc_two: "",
+        en_two_on: "",
+        en_two_tw: "",
+        en_two_th: "",
+        weak: "",
+        str: "",
+        retreat: "",
+        description: "",
+    }
+    res.render('add', {card, types});
+}
+
+module.exports.addCard = async function(req, res) {
+    await Card.create(
+        {
+            name: req.body.name,
+            hp: req.body.hp,
+            type: req.body.type,
+            icon: req.body.icon,
+            picture: req.body.picture,
+            pop: req.body.pop,
+            move_one: req.body.move_one,
+            dam_one: req.body.dam_one,
+            desc_one: req.body.desc_one,
+            en_one_on: req.body.en_one_on,
+            en_one_tw: req.body.en_one_tw,
+            en_one_th: req.body.en_one_th,
+            move_two: req.body.move_two,
+            dam_two: req.body.dam_two,
+            desc_two: req.body.desc_two,
+            en_two_on: req.body.en_two_on,
+            en_two_tw: req.body.en_two_tw,
+            en_two_th: req.body.en_two_th,
+            weak: req.body.weak,
+            str: req.body.str,
+            retreat: req.body.retreat,
+            description: req.body.description
+        });
+    res.redirect('/');
+}
+
 // const cards = [ {
 //     id: 1,
 //     name: 'Eevee',
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,14 @@ const cardController = require('../controllers/cardController');
 /* GET home page. */
 router.get('/', cardController.viewAll);
 
+router.get('/edit/:id', cardController.renderEditForm);
+
+router.post('/edit/:id', cardController.updateCard);
+
+router.get('/delete/:id', cardController.deleteCard);
+
+router.get('/add', cardController.renderAddForm);
+
+router.post('/add', cardController.addCard);
+
 module.exports = router;
